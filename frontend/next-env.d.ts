78b0872f2aa226a966a7f6c790aeb132963b55b4
great-park <cksgh1735@naver.com>
2025-08-21 10:52:41 +0900

    Fix JSX type errors by adding explicit JSX namespace declarations

--- frontend/next-env.d.ts
+++ frontend/next-env.d.ts
@@ -4,5 +4,24 @@
 /// <reference types="react" />
 /// <reference types="react-dom" />
 
+declare namespace JSX {
+  interface IntrinsicElements {
+    div: any;
+    span: any;
+    section: any;
+    h1: any;
+    h2: any;
+    h5: any;
+    h6: any;
+    p: any;
+    button: any;
+    i: any;
+    a: any;
+    br: any;
+    small: any;
+    strong: any;
+  }
+}
+
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
